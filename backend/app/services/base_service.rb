3583f336d30acd1032d7d716f816e446786afadf
takuya-38 <takuya.kogoma.38@gmail.com>
2024-04-08 21:52:03 +0900

    feat: GoogleCalendarのサービスクラス作成

---
+++ backend/app/services/base_service.rb
@@ -0,0 +1,10 @@
+class BaseService
+  # include ActionView::Helpers::TextHelper
+  # include ActionView::Helpers::SanitizeHelper
+
+  # include RoutingHelper
+
+  # def call(*)
+  #   raise NotImplementedError
+  # end
+end
