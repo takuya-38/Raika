4a4fd1198b66e047676fdd6f41b6085625c13003
takuya-38 <takuya.kogoma.38@gmail.com>
2024-04-15 14:38:13 +0900

    feat: events_controllerのindex,createの作成

---
+++ backend/app/controllers/events_controller.rb
@@ -0,0 +1,45 @@
+class EventsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :set_event, only: %i[ update destroy ]
+
+  def index
+    events = Event.all
+    render json: events
+  end
+
+  def create
+    event = Event.new(event_params)
+
+    Event.transaction do
+      if event.save
+        selected_menu_params[:menus].each do |menu|
+          event.selected_menus.create!(menu_id: menu[:id], price: menu[:price])
+          Rails.logger.debug menu
+        end
+        render json: event, status: :created
+      else
+        render json: { errors: event.errors.full_messages }, status: :unprocessable_entity
+        raise ActiveRecord::Rollback
+      end
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:age_group_id, :google_calendar_id, :gender, :total_price)
+    end
+
+    def selected_menu_params
+      params.require(:event).permit(menus: [:id, :price])
+    end
+
+    def record_not_found
+      render json: { error: "Event not found" }, status: :not_found
+    end
+end
---
+++ backend/test/controllers/events_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class EventsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get events_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get events_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get events_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get events_destroy_url
+    assert_response :success
+  end
+end
