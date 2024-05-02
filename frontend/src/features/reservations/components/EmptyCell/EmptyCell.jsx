1ca73d2a545c8481bcd94c5414f2e28b9046915a
takuya-38 <takuya.kogoma.38@gmail.com>
2024-05-02 16:01:47 +0900

    feat: 日時選択処理

--- frontend/src/features/reservations/components/EmptyCell/EmptyCell.jsx
+++ frontend/src/features/reservations/components/EmptyCell/EmptyCell.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../Reservations/Reservations.module.css'
 import { HOUR_LIST } from '../../constants/time'
 
 const EmptyCell = ({ date }) => {
+  const [selectedRange, setSelectedRange] = useState(null)
+  const [isDragging, setIsDragging] = useState(false)
+
+  // ドラッグ開始のハンドラ
+  const handleDragStart = (time) => {
+    setSelectedRange({ start: time, end: time })
+    setIsDragging(true)
+  }
+
+  // ドラッグ中のハンドラ
+  const handleDragEnter = (time) => {
+    if (isDragging) {
+      setSelectedRange((prevRange) => ({
+        ...prevRange,
+        end: time,
+      }))
+    }
+  }
+
+  // ドラッグ終了のハンドラ
+  const handleDragEnd = () => {
+    setIsDragging(false)
+    console.log(selectedRange)
+  }
+
   return (
     <>
       {HOUR_LIST.map((hourList) => (
         <div
-          key={hourList.hour}
-          onClick={() => {
-            console.log(date, `${hourList.time}`)
-          }}
+          key={hourList.time}
+          onMouseDown={() => handleDragStart(hourList.time)}
+          onMouseEnter={() => handleDragEnter(hourList.time)}
+          onMouseUp={handleDragEnd}
           className={styles.empty}
         />
       ))}
