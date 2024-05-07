8f36c7ec7ff4906e630bedc70e6e9014e778b133
takuya-38 <takuya.kogoma.38@gmail.com>
2024-05-07 23:15:43 +0900

    fix: rubyのバージョンを3.3.0に統一

--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -1,14 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  darkMode: 'class',
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    // Path to Tremor module
-    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    transparent: 'transparent',
-    current: 'currentColor',
     extend: {
       colors: {
         // light mode
