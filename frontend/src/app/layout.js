d29ac5dce8050546d7e1a561e7e178a70f2fbc4d
takuya-38 <takuya.kogoma.38@gmail.com>
2024-05-09 22:03:11 +0900

    feat: ポップアップ表示の実装完了

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { SnackbarProvider } from '@/app/components/layouts/SnackbarProvider/SnackbarProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <SnackbarProvider>
+        <body className={inter.className}>{children}</body>
+      </SnackbarProvider>
     </html>
   )
 }
