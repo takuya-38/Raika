4c442e6ce9244bf79b5a38ac8413a77a5b1189fb
takuya-38 <takuya.kogoma.38@gmail.com>
2024-05-13 17:34:43 +0900

    feat: Firebase AuthorizationによるJWT検証(バックエンド)

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  include FirebaseAuthenticator
+  before_action :authenticate
+  class AuthenticationError < StandardError; end
+  rescue_from AuthenticationError, with: :not_authenticated
+
+  def authenticate
+    payload = decode(request.headers["Authorization"]&.split&.last)
+    raise AuthenticationError unless payload["user_id"]
+  end
+
+  # 今後のため
+  # def current_user(user_id = nil)
+  #   @current_user ||= User.find_by(uid: user_id)
+  # end
+
+  private
+    def not_authenticated
+      render json: { error: { messages: ["ログインしてください"] } }, status: :unauthorized
+    end
 end
