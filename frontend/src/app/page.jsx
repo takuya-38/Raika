bfb66101acf0656bce1ba15d7a16fe163f2d83ac
takuya-38 <takuya.kogoma.38@gmail.com>
2024-05-15 21:04:17 +0900

    feat: Googleログインボタンのデザイン実装

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,9 +1,10 @@
 'use client'
-// import { useAuth } from '@/context/auth'
-// import { login, logout } from '@/lib/auth'
-// import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
-import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { GoogleAuthProvider } from 'firebase/auth'
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
+
 import { auth } from '@/lib/FirebaseConfig'
 import { useSnackbarContext } from '@/app/components/layouts/SnackbarProvider/SnackbarProvider'
 
@@ -11,45 +12,70 @@ export default function Home() {
   const router = useRouter()
   const { showSnackbar } = useSnackbarContext()
 
-  const googleProvider = new GoogleAuthProvider()
-  const handleGoogleSignIn = () => {
-    signInWithPopup(auth, googleProvider)
-      .then(() => {
-        if (auth.currentUser.email !== process.env.NEXT_PUBLIC_APPLY_MAIL) {
-          auth.currentUser.delete()
-          if (showSnackbar) {
-            showSnackbar('error', '使用不可なアカウントです。')
-          }
-        } else {
-          console.log(auth.currentUser.email)
-          router.push('/reservations')
-        }
-      })
-      .catch((err) => console.error(err))
-  }
+  // const googleProvider = new GoogleAuthProvider()
+  // const handleGoogleSignIn = () => {
+  //   signInWithPopup(auth, googleProvider)
+  //     .then(() => {
+  //       if (auth.currentUser.email !== process.env.NEXT_PUBLIC_APPLY_MAIL) {
+  //         auth.currentUser.delete()
+  //         if (showSnackbar) {
+  //           showSnackbar('error', '使用不可なアカウントです。')
+  //         }
+  //       } else {
+  //         console.log(auth.currentUser.email)
+  //         router.push('/reservations')
+  //       }
+  //     })
+  //     .catch((err) => console.error(err))
+  // }
 
-  const handleTest = () => {
-    auth.currentUser
-      .getIdToken(true)
-      .then((idToken) => {
-        return fetch('http://localhost:3001/menus', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + idToken,
-          },
-        })
-      })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error('Error fetching events:', err))
-  }
+  // const handleTest = () => {
+  //   auth.currentUser
+  //     .getIdToken(true)
+  //     .then((idToken) => {
+  //       return fetch('http://localhost:3001/menus', {
+  //         method: 'GET',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           Authorization: 'Bearer ' + idToken,
+  //         },
+  //       })
+  //     })
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data))
+  //     .catch((err) => console.error('Error fetching events:', err))
+  // }
+
+  useEffect(() => {
+    const ui =
+      firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth)
+    const uiConfig = {
+      signInOptions: [GoogleAuthProvider.PROVIDER_ID],
+      callbacks: {
+        signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+          const user = authResult.user
+          if (user.email !== process.env.NEXT_PUBLIC_APPLY_MAIL) {
+            user.delete().then(() => {
+              if (showSnackbar) {
+                showSnackbar('error', '使用不可なアカウントです。')
+              }
+            })
+          } else {
+            console.log(user.email)
+            router.push('/reservations')
+          }
+          return false
+        },
+      },
+      signInFlow: 'popup',
+    }
+    ui.start('#firebaseui-auth-container', uiConfig)
+  }, [showSnackbar])
 
   return (
-    <>
-      <p>Sign in with Google:</p>
-      <button onClick={handleGoogleSignIn}>Google</button>
-      <button onClick={handleTest}>test</button>
-    </>
+    <div>
+      <h1>Welcome to My App</h1>
+      <div className="googleLogo" id="firebaseui-auth-container"></div>
+    </div>
   )
 }
