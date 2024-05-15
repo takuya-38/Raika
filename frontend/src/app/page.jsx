76e32a36cbe8171ecf91591a2821a7aa462de0b1
takuya-38 <takuya.kogoma.38@gmail.com>
2024-05-15 22:52:53 +0900

    feat: ログイン画面のデザイン完成

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,50 +1,20 @@
 'use client'
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
 import { GoogleAuthProvider } from 'firebase/auth'
 import * as firebaseui from 'firebaseui'
 import 'firebaseui/dist/firebaseui.css'
 
+import logoImage from '@/public/logo/Raika_logo.png'
+import pictureImage from '@/public/picture/login_page_picture.jpg'
 import { auth } from '@/lib/FirebaseConfig'
 import { useSnackbarContext } from '@/app/components/layouts/SnackbarProvider/SnackbarProvider'
+import styles from './style.module.css'
 
 export default function Home() {
   const router = useRouter()
   const { showSnackbar } = useSnackbarContext()
-
-  // const googleProvider = new GoogleAuthProvider()
-  // const handleGoogleSignIn = () => {
-  //   signInWithPopup(auth, googleProvider)
-  //     .then(() => {
-  //       if (auth.currentUser.email !== process.env.NEXT_PUBLIC_APPLY_MAIL) {
-  //         auth.currentUser.delete()
-  //         if (showSnackbar) {
-  //           showSnackbar('error', '使用不可なアカウントです。')
-  //         }
-  //       } else {
-  //         console.log(auth.currentUser.email)
-  //         router.push('/reservations')
-  //       }
-  //     })
-  //     .catch((err) => console.error(err))
-  // }
-
-  // const handleTest = () => {
-  //   auth.currentUser
-  //     .getIdToken(true)
-  //     .then((idToken) => {
-  //       return fetch('http://localhost:3001/menus', {
-  //         method: 'GET',
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //           Authorization: 'Bearer ' + idToken,
-  //         },
-  //       })
-  //     })
-  //     .then((res) => res.json())
-  //     .then((data) => console.log(data))
-  //     .catch((err) => console.error('Error fetching events:', err))
-  // }
 
   useEffect(() => {
     const ui =
@@ -73,9 +43,28 @@ export default function Home() {
   }, [showSnackbar])
 
   return (
-    <div>
-      <h1>Welcome to My App</h1>
-      <div className="googleLogo" id="firebaseui-auth-container"></div>
+    <div className={styles.container}>
+      <div className={styles.loginPage}>
+        <div className={styles.pictureWrapper}>
+          <Image
+            src={pictureImage}
+            alt="picture"
+            layout="fill"
+            className={styles.pictureImage}
+          />
+          <div className={styles.overlay}></div>
+        </div>
+
+        <div className={styles.loginWrapper}>
+          <div className={styles.imageBox}>
+            <Image src={logoImage} alt="logo" width="auto" height={90} />
+          </div>
+          <div
+            className={styles.googleLogo}
+            id="firebaseui-auth-container"
+          ></div>
+        </div>
+      </div>
     </div>
   )
 }
