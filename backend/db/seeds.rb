1aed497dbe1980dbf65e1e72521c4a19016c889b
takuya-38 <takuya.kogoma.38@gmail.com>
2024-05-18 00:05:31 +0900

    feat: seedファイル作成

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -36,3 +36,57 @@ group_names.each do |name|
   )
 end
 
+# Sales
+1700.times do
+  Sale.create(
+    age_group_id: rand(1..7),
+    google_calendar_id: 1,
+    gender: rand(1..2),
+    created_at: Faker::Date.between(from: '2018-01-01', to: '2023-12-31'),
+    updated_at: Time.current
+  )
+end
+
+300.times do
+  Sale.create(
+    age_group_id: rand(1..7),
+    google_calendar_id: 1,
+    gender: rand(1..2),
+    created_at: Faker::Date.between(from: '2024-01-01', to: '2024-05-17'),
+    updated_at: Time.current
+  )
+end
+
+# SelectedMenu
+menu_prices = {
+  1 => 5500,
+  2 => 550,
+  3 => 1100,
+  4 => 1980,
+  5 => 5830,
+  6 => 5830,
+  7 => 5830,
+  8 => 550,
+  9 => 16500,
+  10 => 2200,
+  11 => 2750,
+  12 => 3300
+}
+
+2000.times do |i|
+  menu_id = rand(1..12)
+  SelectedMenu.create(
+    menu_id: menu_id,
+    sale_id: i + 1,
+    price: menu_prices[menu_id]
+  )
+end
+
+500.times do |i|
+  menu_id = rand(1..12)
+  SelectedMenu.create(
+    menu_id: menu_id,
+    sale_id: rand(1..2000),
+    price: menu_prices[menu_id]
+  )
+end
