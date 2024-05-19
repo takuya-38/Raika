4713770da202c32bca06677f6ba344bdedba9369
takuya-38 <takuya.kogoma.38@gmail.com>
2024-05-19 10:32:08 +0900

    feat: ヘルスチェックを有効にする設定

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::API
   def authenticate
     Rails.logger.info(request.headers["Authorization"]&.split&.last)
     payload = decode(request.headers["Authorization"]&.split&.last)
-    # Rails.logger.info(payload)
     raise AuthenticationError unless payload["user_id"]
   end
 
